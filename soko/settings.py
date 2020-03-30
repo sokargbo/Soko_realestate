a8b8787c664d30b66c8304584747d67c190b16bb
morleh sokargbo <msokargbo@diux.mil@morlehs-MacBook-Air.local>
2020-03-29 18:15:16 -0700

    Removed emails from settings

--- soko/settings.py
+++ soko/settings.py
@@ -143,12 +143,7 @@ MEDIA_URL = '/media/'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
-# Email Config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
+
 
 try:
     from .local_settings import *
